8b8d72284083ae3448ba17eb4f0ab3a76c827d1f
abma <spam@abma.de>
2010-09-08 02:15:30 +0200

    print filename from torrent

--- src/Downloader/Torrent/TorrentDownloader.cpp
+++ src/Downloader/Torrent/TorrentDownloader.cpp
@@ -15,7 +15,9 @@ void CTorrentDownloader::start(IDownload* download){
 	printf("%s %s:%d \n",__FILE__, __FUNCTION__ ,__LINE__);
 	if (download==NULL)
 		return;
-	libtorrent::session s;
+	libtorrent::session* s;
+
+	s=new libtorrent::session();
 
     libtorrent::session_settings setting;
     setting.tracker_completion_timeout=1;
@@ -23,15 +25,20 @@ void CTorrentDownloader::start(IDownload* download){
     setting.peer_timeout=1;
     setting.urlseed_timeout=1;
 
-	s.set_settings(setting);
+	s->set_settings(setting);
 
-	s.listen_on(std::make_pair(6881, 6889));
+	s->listen_on(std::make_pair(6881, 6889));
 
 	libtorrent::add_torrent_params p;
 	p.save_path = download->name; //name contains the path, because torrents already include the filenames
     p.ti = new libtorrent::torrent_info(download->url.c_str());
+	for (int i=0; i<p.ti->num_files(); i++){
+		printf("File %d in torrent: %s\n",i, p.ti->file_at(i).path.filename().c_str());
+	}
+
+
     printf("Downloading torrent to %s\n", download->name.c_str());
-    libtorrent::torrent_handle tor=s.add_torrent(p);
+    libtorrent::torrent_handle tor=s->add_torrent(p);
 
     std::list<std::string>::iterator it;
 	for(it=download->mirror.begin(); it!=download->mirror.end(); it++){
@@ -40,16 +47,17 @@ void CTorrentDownloader::start(IDownload* download){
 	}
 
     while( (!tor.is_seed()) && (tor.is_valid())){
-    	const libtorrent::session_status& sessinfo=s.status();
+		const libtorrent::session_status& sessinfo=s->status();
     	sleep(1);
     	printf("%ld\n",sessinfo.total_download);
 
     }
 
+    printf("download finished, shuting down torrent...\n");
+    s->pause();
+	delete(s);
+	printf("shut down!\n");
 
-	s.abort();
-
-    printf("download finished\n");
 }
 
 const IDownload* CTorrentDownloader::addDownload(const std::string& url, const std::string& filename){
