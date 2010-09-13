10145647b6c479d714a6e381241537633ba8f4a5
abma <spam@abma.de>
2010-09-14 01:59:45 +0200

    updated release-tool (store also debug symbols)

--- tools/release.sh
+++ tools/release.sh
@@ -1,12 +1,19 @@
 #!/bin/sh
 
 cd ..
-
+make -j8 install
 cd dist
 FILES=`ls *.dll *.exe`
 for i in $FILES; do
 	echo Strip $i
-	i586-mingw32msvc-strip $i
+	i586-mingw32msvc-objcopy --only-keep-debug $i $i.dbg
+	i586-mingw32msvc-strip --strip-unneeded $i
 done
+FILESDBG=`ls *.dbg`
+ZIP="7z a -t7z -m0=lzma -mx=9 -mfb=64 -md=32m -ms=on"
 
-7z a -tzip -mx=9 rapid.zip $FILES
+${ZIP} rapid.7z $FILES
+ls -lh rapid.7z
+${ZIP} rapid_dbg.7z $FILESDBG
+ls -lh rapid_dbg.7z
+
