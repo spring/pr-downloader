eaa61a3e481f0ca198ed7457b9d987677943cb6f
matze <matze@abma.de>
2010-09-19 22:49:27 +0200

    show --help as default, update todo, --filesystem--springpath

--- src/main.cpp
+++ src/main.cpp
@@ -18,32 +18,56 @@
 #include <stdio.h>
 #include <list>
 
+enum{
+	RAPID_DOWNLOAD=0,
+	RAPID_VALIDATE,
+	RAPID_LIST,
+	PLASMA_DOWNLOAD,
+	PLASMA_SEARCH,
+	TORRENT_DOWNLOAD,
+	WIDGET_SEARCH,
+	FILESYSTEM_WRITEPATH,
+	HELP
+};
+
+static struct option long_options[] = {
+	{"rapid-download"          , 1, 0, RAPID_DOWNLOAD},
+	{"rapid-validate"          , 0, 0, RAPID_VALIDATE},
+	{"rapid-list"              , 0, 0, RAPID_LIST},
+	{"plasma-download"         , 1, 0, PLASMA_DOWNLOAD},
+	{"plasma-search"           , 1, 0, PLASMA_SEARCH},
+	{"torrent-download"        , 1, 0, TORRENT_DOWNLOAD},
+	{"widget-search"           , 1, 0, WIDGET_SEARCH},
+	{"filesystem-writepath"    , 0, 0, FILESYSTEM_WRITEPATH},
+	{"help"                    , 0, 0, HELP},
+	{0                         , 0, 0, 0}
+};
+
+void show_help(const char* cmd){
+	int i=0;
+	printf("Usage: %s ", cmd);
+	bool append=false;
+	while(long_options[i].name!=0){
+		if (append){
+			printf("\n");
+		}
+		append=true;
+		printf("--%s",long_options[i].name);
+		if (long_options[i].has_arg!=0)
+			printf(" <name>");
+		i++;
+	}
+	printf("\n");
+	exit(1);
+}
 
 int main(int argc, char **argv){
-	enum{
-		RAPID_DOWNLOAD=0,
-		RAPID_VALIDATE,
-		RAPID_LIST,
-		PLASMA_DOWNLOAD,
-		PLASMA_SEARCH,
-		TORRENT_DOWNLOAD,
-		WIDGET_SEARCH,
-		HELP
-	};
+	if (argc<2)
+		show_help(argv[0]);
 
-	static struct option long_options[] = {
-		{"rapid-download"   , 1, 0, RAPID_DOWNLOAD},
-		{"rapid-validate"   , 0, 0, RAPID_VALIDATE},
-		{"rapid-list"       , 0, 0, RAPID_LIST},
-		{"plasma-download"  , 1, 0, PLASMA_DOWNLOAD},
-		{"plasma-search"    , 1, 0, PLASMA_SEARCH},
-		{"torrent-download" , 1, 0, TORRENT_DOWNLOAD},
-		{"widget-search"    , 1, 0, WIDGET_SEARCH},
-		{"help"             , 0, 0, HELP},
-		{0                  , 0, 0, 0}
-	};
 	CFileSystem::Initialize();
 	IDownloader::Initialize();
+		
 	while(true){
 		int option_index = 0;
 		int c = getopt_long(argc, argv, "",long_options, &option_index);
@@ -93,23 +117,13 @@ int main(int argc, char **argv){
 				widgetDownload->search(tmp);
 				break;
 			}
+			case FILESYSTEM_WRITEPATH: {
+				printf("%s\n",fileSystem->getSpringDir().c_str());
+				break;
+			}
 			case HELP:
-			default:{
-				int i=0;
-				printf("Usage: %s ", argv[0]);
-				bool append=false;
-				while(long_options[i].name!=0){
-					if (append){
-						printf("|");
-					}
-					append=true;
-					printf("--%s",long_options[i].name);
-					if (long_options[i].has_arg!=0)
-						printf(" <name>");
-					i++;
-				}
-				printf("\n");
-				exit(1);
+			default: {
+				show_help(argv[0]);
 				break;
 			}
 		}
