ab2c0d448bcaf1afa7762d2c68a86adc9b16667e
abma <spring@abma.de>
2011-10-07 00:22:44 +0200

    prototype for file i/o abstraction

--- src/FileSystem/File.cpp
+++ src/FileSystem/File.cpp
@@ -1,3 +1,62 @@
 #include "File.h"
+#include "FileSystem.h"
 
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+
+CFile::CFile(const std::string& filename, int size, int piecesize)
+{
+//	fileSystem->createSubdirs(filename);
+	if (fileSystem->fileExists(filename))
+		handle=open(filename.c_str(), O_CREAT);
+	this->piecesize=piecesize;
+	this->size=size;
+	if ( (size>0) && (piecesize>0)) {
+		const int pieces = size / piecesize;
+		for (int i=0; i<pieces; i++) {
+			this->pieces[i]=CPiece();
+		}
+
+	}
+}
+
+CFile::~CFile()
+{
+	//TODO: write buffered data
+	close(handle);
+}
+
+bool CFile::Validate(std::map <std::string, IHash> hashs, int piece)
+{
+//	if (piece>=0)
+	//FIXME
+	return true;
+}
+
+int CFile::Read(char*buf, int bufsize, int piece)
+{
+//	if (piece>=0)
+	int bytes=read(handle, buf, bufsize);
+	return bytes;
+}
+
+int CFile::Write(const char*buf, int bufsize, int piece)
+{
+	if (piece>=0) {
+		const CPiece& ppiece=getPiece(piece);
+		Seek(ppiece.pos, piece);
+	}
+	int bytes=write(handle, buf, bufsize);
+	return bytes;
+}
+
+int CFile::Seek(int pos, int piece)
+{
+//	if (piece>=0)
+	int offset=lseek(handle, pos, SEEK_SET);
+	return offset;
+}
 
--- src/FileSystem/File.h
+++ src/FileSystem/File.h
@@ -1,30 +1,56 @@
 #ifndef _FILE_H_
 #define _FILE_H_
 
-#include <list>
+#include <string>
+#include <map>
 
-class CChecksum;
+class IHash;
 
 
 class CPiece
 {
-	CChecksum& checksum;
-	//checksum;
+public:
+	std::map <std::string, IHash*> hashs; //checksum for a part of a file
+	bool validate(std::map <std::string, IHash*> hashs);
 	int size;
 	int written; //bytes written
 	bool valid; //checksum validated
+	int pos; //current read/write pos
 };
 
 class CFile
 {
-	CFile();
-
-
+	/**
+	* general file abstraction, allows to write to pieces of a file and create hashes
+	* of the complete fiele or pieces of it
+	*/
+	/**
+	*	create a new file
+	*	@param filename filename of the file
+	*	@param size of the filename, -1 will read it from file or create a new one
+	*/
+	CFile(const std::string& filename, int size=-1, int piecesize=-1);
+	~CFile();
+	bool Validate(std::map <std::string, IHash> hashs, int piece=-1);
+	int Read(char* buf, int bufsize, int piece=-1);
+	int Write(const char* buf, int bufsize, int piece=-1);
+	int Seek(int pos, int piece=-1);
+	bool SetPieceSize(int size);
+	const CPiece& getPiece(int piece);
+	//int Open(const std::string& filename);
+	//int Close();
+	/**
+	*	set the size of a pice
+	* @return count of pieces
+	*/
+	int setpiecesieze(int size);
 
 private:
-	int handle;
-	std::list<CPiece> pieces;
-
+	int handle; //file handle
+	int piecesize; //size of a piece
+	int size; //file size
+	std::map <int, CPiece> pieces; //pieces of the file
+	std::map <std::string, IHash*> hashs; //checksums for the complete file
 
 };
 
