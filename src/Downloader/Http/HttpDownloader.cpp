cf5c724ea20d6bd23a4ba1e560171eae5dec4e0d
abma <spring@abma.de>
2012-05-28 20:57:32 +0200

    oops, fix mirror selection

--- src/Downloader/Http/HttpDownloader.cpp
+++ src/Downloader/Http/HttpDownloader.cpp
@@ -324,6 +324,7 @@ bool CHttpDownloader::processMessages(CURLM* curlm, std::vector <DownloadData*>&
 			double dlSpeed;
 			curl_easy_getinfo(data->easy_handle, CURLINFO_SPEED_DOWNLOAD, &dlSpeed);
 			data->mirror->UpdateSpeed(dlSpeed);
+			data->mirror->status=Mirror::STATUS_OK;
 
 			//remove easy handle, as its finished
 			curl_multi_remove_handle(curlm, data->easy_handle);
