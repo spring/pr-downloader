9099fb945e9bd3a7a1e1588a1bf19283e8ecdd59
abma <spring@abma.de>
2012-08-23 18:56:35 +0200

    fix #39: warning: deleting object of polymorphic class type 'CFile' which has non-virtual destructor might cause undefined behaviour + style

--- src/FileSystem/IFile.h
+++ src/FileSystem/IFile.h
@@ -6,6 +6,8 @@
 class IFile
 {
 public:
+	virtual ~IFile() {}
+
 	/**
 	* general file abstraction, allows to write to pieces of a file and create hashes
 	* of the complete fiele or pieces of it
@@ -14,19 +16,25 @@ public:
 	*	create a new file
 	*	@param filename filename of the file
 	*/
-    virtual bool Open(const std::string& /*filename*/){return false;}
+	virtual bool Open(const std::string& /*filename*/) {
+		return false;
+	}
 	/**
 	*	close file
 	*/
-	virtual void Close(){}
+	virtual void Close() {}
 	/**
 	*	read buf from file, starting at restored piece pos, if piece>=0
 	*/
-    virtual int Read(char* /*buf*/, int /*bufsize*/){return 0;}
+	virtual int Read(char* /*buf*/, int /*bufsize*/) {
+		return 0;
+	}
 	/**
 	*	write buf to file, starting at last pos restored from piece, if piece>=0
 	*/
-    virtual int Write(const char* /*buf*/, int /*bufsize*/){return 0;}
+	virtual int Write(const char* /*buf*/, int /*bufsize*/) {
+		return 0;
+	}
 };
 
 #endif
