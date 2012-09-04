4cc83cd7b0e89a1ce7b5bc999c027928a16c97db
abma <spring@abma.de>
2012-09-04 23:39:31 +0200

    fix some compile warnings (unused func/not initialized var)

--- src/lib/minizip/zip.c
+++ src/lib/minizip/zip.c
@@ -189,13 +189,14 @@ local void init_linkedlist(ll)
     ll->first_block = ll->last_block = NULL;
 }
 
+/*
 local void free_linkedlist(ll)
     linkedlist_data* ll;
 {
     free_datablock(ll->first_block);
     ll->first_block = ll->last_block = NULL;
 }
-
+*/
 
 local int add_data_in_datablock(ll,buf,len)
     linkedlist_data* ll;
@@ -373,7 +374,7 @@ local int ziplocal_getShort (pzlib_filefunc_def,filestream,pX)
     uLong *pX;
 {
     uLong x ;
-    int i;
+    int i=0;
     int err;
 
     err = ziplocal_getByte(pzlib_filefunc_def,filestream,&i);
@@ -401,7 +402,7 @@ local int ziplocal_getLong (pzlib_filefunc_def,filestream,pX)
     uLong *pX;
 {
     uLong x ;
-    int i;
+    int i=0;
     int err;
 
     err = ziplocal_getByte(pzlib_filefunc_def,filestream,&i);
