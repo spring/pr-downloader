spring/pr-downloader

commit cf97c5d4398413011044e2b25a02ebb3d3f77244

merge RepoMaster.* into RapidDownloader.* && add setMasterUrl

src/Downloader/Rapid/Sdp.cpp
@@ -2,7 +2,6 @@
 
 #include "Sdp.h"
 #include "RapidDownloader.h"
-#include "RepoMaster.h"
 #include "Util.h"
 #include "Logger.h"
 #include "FileSystem/FileSystem.h"
@@ -56,7 +55,7 @@ bool CSdp::download()
 
 	if (!fileSystem->fileExists(filename)) { //.sdp isn't avaiable, download it
 		IDownload dl(filename);
-		dl.addMirror(url + "/packages/" + this->md5 + ".sdp");
+		dl.addMirror(url + PATH_DELIMITER + "packages"+ PATH_DELIMITER + this->md5 + ".sdp");
 		httpDownload->download(&dl);
 	}
 	fileSystem->parseSdp(filename,files); //parse downloaded file
@@ -66,7 +65,7 @@ bool CSdp::download()
 	HashMD5 md5= HashMD5();
 	md5.Set(tmp.md5, sizeof(tmp.md5));
 	int i=0;
-	for(it = files.begin(); it!=files.end(); ++it) {
+	for(it = files.begin(); it!=files.end(); ++it) { //check which file are available on local disk -> create list of files to download
 		i++;
 		md5.Set((*it)->md5, sizeof((*it)->md5));
 		std::string file;
@@ -89,7 +88,8 @@ bool CSdp::download()
 		LOG_DEBUG("Already downloaded: %s", shortname.c_str());
 		downloaded=true;
 	}
-	for(it = files.begin(); it!=files.end(); ++it) {
+
+	for(it = files.begin(); it!=files.end(); ++it) { //free memory
 		delete *it;
 	}
 	return downloaded;

src/Downloader/Rapid/CMakeLists.txt
@@ -1,7 +1,6 @@
 add_library(Rapid STATIC
 	RapidDownloader.cpp
 	Repo.cpp
-	RepoMaster.cpp
 	Sdp.cpp
 )
 

src/Downloader/Rapid/RapidDownloader.cpp
@@ -4,29 +4,30 @@
 #include "FileSystem/FileSystem.h"
 #include "Util.h"
 #include "Logger.h"
-#include "RepoMaster.h"
+#include "Repo.h"
 #include "Sdp.h"
 
 #include <stdio.h>
 #include <string>
 #include <string.h>
 #include <list>
+#include <zlib.h>
+
 #ifndef WIN32
 #include <regex.h>
 #endif
 
 
-CRapidDownloader::CRapidDownloader(const std::string& url)
+CRapidDownloader::CRapidDownloader()
 {
-	repoMaster=new CRepoMaster(url, this);
 	reposLoaded = false;
 	sdps.clear();
+	setMasterUrl(REPO_MASTER);
 }
 
 CRapidDownloader::~CRapidDownloader()
 {
 	sdps.clear();
-	delete repoMaster;
 }
 
 
@@ -58,7 +59,7 @@ bool CRapidDownloader::reloadRepos()
 {
 	if (reposLoaded)
 		return true;
-	repoMaster->updateRepos();
+	updateRepos();
 	reposLoaded=true;
 	return true;
 }
@@ -133,3 +134,75 @@ bool CRapidDownloader::match_download_name(const std::string &str1,const std::st
 	*/
 	return false;
 }
+
+void CRapidDownloader::setMasterUrl(const std::string& url)
+{
+	this->url=url;
+	reposLoaded = false;
+}
+
+
+void CRapidDownloader::download(const std::string& name)
+{
+	std::string tmp;
+	urlToPath(name,tmp);
+	this->path = fileSystem->getSpringDir() + PATH_DELIMITER +"rapid" +PATH_DELIMITER+ tmp;
+	fileSystem->createSubdirs(path);
+	LOG_DEBUG("%s",name.c_str());
+	//first try already downloaded file, as repo master file rarely changes
+	if ((fileSystem->fileExists(path)) && (fileSystem->isOlder(path,REPO_MASTER_RECHECK_TIME)) && parse())
+		return;
+	IDownload dl(path);
+	dl.addMirror(name);
+	httpDownload->download(&dl);
+	parse();
+}
+
+bool CRapidDownloader::parse()
+{
+	gzFile fp=gzopen(path.c_str(), "r");
+	if (fp==Z_NULL) {
+		LOG_ERROR("Could not open %s",path.c_str());
+		return false;
+	}
+	char buf[IO_BUF_SIZE];
+	repos.clear();
+	int i=0;
+	while (gzgets(fp, buf, sizeof(buf))!=Z_NULL) {
+		std::string tmp=buf;
+		std::string url=getStrByIdx(tmp,',',1);
+		i++;
+		if (url.size()>0) { //create new repo from url
+			CRepo repotmp=CRepo(url, this);
+			repos.push_back(repotmp);
+		} else {
+			LOG_ERROR("Parse Error %s, Line %d: %s",path.c_str(),i,buf);
+			return false;
+		}
+	}
+	gzclose(fp);
+	LOG_INFO("Found %d repos in %s",repos.size(),path.c_str());
+	return true;
+}
+
+void CRapidDownloader::updateRepos()
+{
+	LOG_DEBUG("%s","Updating repos...");
+	download(url);
+	std::list<CRepo>::iterator it;
+	std::list<IDownload*> dls;
+	for (it = repos.begin(); it != repos.end(); ++it) {
+		IDownload* dl = new IDownload();
+		if ((*it).getDownload(*dl)) {
+			dls.push_back(dl);
+		} else {
+			delete dl;
+		}
+	}
+	httpDownload->download(dls);
+	for (it = repos.begin(); it != repos.end(); ++it) {
+		(*it).parse();
+	}
+	IDownloader::freeResult(dls);
+}
+

src/Downloader/Rapid/RapidDownloader.h
@@ -8,20 +8,19 @@
 #include <string>
 #include <list>
 #include <stdio.h>
-//FIXME: read from config / use this as default
-#define REPO_MASTER "http://repos.springrts.com/repos.gz"
 #define REPO_MASTER_RECHECK_TIME 86400 //how long to cache the repo-master file in secs without rechecking
 #define REPO_RECHECK_TIME 600
+#define REPO_MASTER "http://repos.springrts.com/repos.gz"
 
 class CSdp;
 class CHttpDownload;
 class CFileSystem;
-class CRepoMaster;
+class CRepo;
 
 class CRapidDownloader: public IDownloader
 {
 public:
-	CRapidDownloader(const std::string& masterurl=REPO_MASTER);
+	CRapidDownloader();
 	~CRapidDownloader();
 
 	/**
@@ -45,7 +44,20 @@ public:
 	*/
 	virtual bool download(IDownload* download);
 
+	void setMasterUrl(const std::string& url);
+	/**
+		parses a rep master-file
+	*/
+	void updateRepos();
+	void downloadRepo(const std::string& url);
+
 private:
+	bool parse();
+	void download(const std::string& name);
+	std::string path;
+	std::string url;
+	std::list<CRepo> repos;
+
 	/**
 		download by name, for example "Complete Annihilation revision 1234"
 	*/
@@ -66,7 +78,6 @@ private:
 	static bool match_download_name(const std::string &str1,const std::string& str2);
 
 	static bool list_compare(CSdp& first ,CSdp& second);
-	CRepoMaster* repoMaster;
 	std::list<CSdp> sdps;
 };
 

src/Downloader/Rapid/Repo.cpp
@@ -1,7 +1,6 @@
 /* This file is part of pr-downloader (GPL v2 or later), see the LICENSE file */
 
 #include "Repo.h"
-#include "RepoMaster.h"
 #include "FileSystem/FileSystem.h"
 #include "Downloader/IDownloader.h"
 #include "RapidDownloader.h"

src/Downloader/Rapid/RepoMaster.cpp
@@ -1,89 +1,0 @@
-/* This file is part of pr-downloader (GPL v2 or later), see the LICENSE file */
-
-#include "RepoMaster.h"
-#include "RapidDownloader.h"
-#include "FileSystem/FileSystem.h"
-#include "Util.h"
-#include "Repo.h"
-#include "Logger.h"
-#include "Downloader/IDownloader.h"
-
-#include <string>
-#include <stdio.h>
-#include <zlib.h>
-
-void CRepoMaster::download(const std::string& name)
-{
-	std::string tmp;
-	urlToPath(name,tmp);
-	this->path = fileSystem->getSpringDir() + PATH_DELIMITER +"rapid" +PATH_DELIMITER+ tmp;
-	fileSystem->createSubdirs(path);
-	LOG_DEBUG("%s",name.c_str());
-	//first try already downloaded file, as repo master file rarely changes
-	if ((fileSystem->fileExists(path)) && (fileSystem->isOlder(path,REPO_MASTER_RECHECK_TIME)) && parse())
-		return;
-	IDownload dl(path);
-	dl.addMirror(name);
-	httpDownload->download(&dl);
-	parse();
-}
-
-CRepoMaster::CRepoMaster(const std::string& url, CRapidDownloader* rapid):
-	rapid(rapid),
-	url(url)
-{
-	LOG_DEBUG("Added master repo %s", url.c_str());
-}
-
-bool CRepoMaster::parse()
-{
-	gzFile fp=gzopen(path.c_str(), "r");
-	if (fp==Z_NULL) {
-		LOG_ERROR("Could not open %s",path.c_str());
-		return false;
-	}
-	char buf[IO_BUF_SIZE];
-	repos.clear();
-	int i=0;
-	while (gzgets(fp, buf, sizeof(buf))!=Z_NULL) {
-		std::string tmp=buf;
-		std::string url=getStrByIdx(tmp,',',1);
-		i++;
-		if (url.size()>0) { //create new repo from url
-			CRepo repotmp=CRepo(url, rapid);
-			repos.push_back(repotmp);
-		} else {
-			LOG_ERROR("Parse Error %s, Line %d: %s",path.c_str(),i,buf);
-			return false;
-		}
-	}
-	gzclose(fp);
-	LOG_INFO("Found %d repos in %s",repos.size(),path.c_str());
-	return true;
-}
-
-void CRepoMaster::updateRepos()
-{
-	LOG_DEBUG("%s","Updating repos...");
-	download(url);
-	std::list<CRepo>::iterator it;
-	std::list<IDownload*> dls;
-	for (it = repos.begin(); it != repos.end(); ++it) {
-		IDownload* dl = new IDownload();
-		if ((*it).getDownload(*dl)) {
-			dls.push_back(dl);
-		} else {
-			delete dl;
-		}
-	}
-	httpDownload->download(dls);
-	for (it = repos.begin(); it != repos.end(); ++it) {
-		(*it).parse();
-	}
-	IDownloader::freeResult(dls);
-}
-
-CRepoMaster::~CRepoMaster()
-{
-
-}

src/Downloader/Rapid/RepoMaster.h
@@ -1,33 +1,0 @@
-/* This file is part of pr-downloader (GPL v2 or later), see the LICENSE file */
-
-#ifndef REPO_MASTER_H
-#define REPO_MASTER_H
-
-#include <string>
-#include <list>
-#include <stdio.h>
-
-class CRepo;
-class CRapidDownloader;
-
-class CRepoMaster
-{
-	CRapidDownloader* rapid;
-public:
-	CRepoMaster(const std::string& masterurl, CRapidDownloader* rapid);
-	~CRepoMaster();
-	/**
-		parses a rep master-file
-	*/
-	void updateRepos();
-	void downloadRepo(const std::string& url);
-private:
-	bool parse();
-	void download(const std::string& name);
-	std::string path;
-	std::string url;
-	std::list<CRepo> repos;
-
-};
-
-#endif

commit 4c80001bdbad76967886c455b3ddc7711965df51

fix wrong url introduced in cf97c5d4398413011044e2b25a02ebb3d3f77244

src/Downloader/Rapid/Sdp.cpp
@@ -70,7 +70,7 @@ bool CSdp::download()
 
 	if (!fileSystem->fileExists(filename)) { //.sdp isn't avaiable, download it
 		IDownload dl(filename + ".tmp");
-		dl.addMirror(url + PATH_DELIMITER + "packages"+ PATH_DELIMITER + this->md5 + ".sdp");
+		dl.addMirror(url + "/packages/" + this->md5 + ".sdp");
 		httpDownload->download(&dl);
 	}
 	fileSystem->parseSdp(filename,files); //parse downloaded file