def79eda195fb2d42841610b1815ce74070720f8
abma <spring@abma.de>
2015-07-15 21:45:12 +0200

    try to workarround broken proxy

--- src/Downloader/CurlWrapper.cpp
+++ src/Downloader/CurlWrapper.cpp
@@ -19,5 +19,12 @@ CURL* CurlWrapper::CurlInit()
 	curl_easy_setopt(ret, CURLOPT_USERAGENT, getVersion());
 	curl_easy_setopt(ret, CURLOPT_FAILONERROR, true);
 	curl_easy_setopt(ret, CURLOPT_FOLLOWLOCATION, 1);
+
+	struct curl_slist *list = NULL;
+        list = curl_slist_append(list, "Cache-Control: no-cache");
+        curl_easy_setopt(ret, CURLOPT_HTTPHEADER, list);
+//FIXME: memleak, has to be freed at shutdown
+//        curl_slist_free_all(list); /* free the list again */
+
 	return ret;
 }
