6286923a0cba7882e379231cc815ed5394cb4fce
abma <spring@abma.de>
2019-03-06 20:03:41 +0100

    update apache config script

--- scripts/update-apache-config.sh
+++ scripts/update-apache-config.sh
@@ -2,9 +2,13 @@
 
 
 GITROOT=/home/packages/git
-WWWROOT=/home/packages/www
+WWWROOT=/home/packages/www/repos.springrts.com
 STREAMER_BIN=/home/packages/bin/Streamer
 DOMAINS=$(find $GITROOT -maxdepth 1 -mindepth 1 -type d)
+# 001- is prefixed for defined order of files
+CONFIGFILE=/etc/apache2/sites-enabled/001-all.repo.springrts.com.conf
+
+echo "" > $CONFIGFILE
 
 for DOMAIN in $DOMAINS; do
 
@@ -28,11 +32,10 @@ User-agent: *
 Disallow: /
 EOF
 
-# 001- is prefixed for defined order of files
-FILE=/etc/apache2/sites-enabled/001-$TAG.repo.springrts.com.conf
 
-echo "Creating $FILE"
-cat >$FILE << EOF
+
+echo "Appending $CONFIGFILE"
+cat >>$CONFIGFILE << EOF
 
 # don't edit! automaticly created with $(realpath $0)
 <VirtualHost *:80>
