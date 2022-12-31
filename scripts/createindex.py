6dad98ee3d07fb15ea3f3a5c4556cbde460abc46
abma <spring@abma.de>
2022-12-31 14:08:38 +0100

    add createindex.py

---
+++ scripts/createindex.py
@@ -0,0 +1,43 @@
+import os
+import json
+from lupa import LuaRuntime
+from configparser import ConfigParser
+
+def get_remote(repodir):
+	configfile = os.path.join(repodir, ".git/config")
+	c = ConfigParser()
+	c.read(configfile)
+	return c.get('remote "origin"', "url")
+
+def get_name(repodir):
+	modinfo = os.path.join(repodir, "modinfo.lua")
+	lua = LuaRuntime()
+	with open(modinfo, "r") as f:
+		content = f.read()
+		l = lua.execute(content)
+		return l["name"]
+
+repolist = {}
+
+for d in os.listdir("."):
+	if not os.path.isdir(d):
+		continue
+	abspath = os.path.abspath(d)
+	reponame = os.path.basename(abspath)
+	url = get_remote(abspath)
+	name = get_name(abspath)
+	assert not reponame in repolist 
+	repolist[reponame] = {
+		"names": [name],
+		"url": url,
+		"stablebranch": "master",
+		"testbranches": ["master"],
+	}
+
+
+repos = {
+	"repos": repolist
+}
+
+with open("repos.json", "w") as f:
+	f.write(json.dumps(repos, indent='\t'))
