7a3fab3ce28326d486268d646c1daa8d51951d2b
abma <spring@abma.de>
2023-01-01 15:32:46 +0100

    ignore ret of git_libgit2_init()

--- src/BuildGit.cpp
+++ src/BuildGit.cpp
@@ -278,7 +278,7 @@ void buildGit(
 	std::string const & Prefix)
 {
 	// Initialize libgit2
-	checkRet(git_libgit2_init() == 1 ? 0 : -1, "git_libgit2_init()");
+	git_libgit2_init();
 	auto && ThreadsGuard = makeScopeGuard([&] { git_libgit2_shutdown(); });
 
 	// Load the git repo
